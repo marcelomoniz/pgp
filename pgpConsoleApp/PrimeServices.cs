7613c485fbea87ec4453eb7243f54c85c5622955
Marcelo Moniz <marcelomoniz@Fiotec.com>
2020-06-22 22:18:20 -0300

    adiciona solution e 2 projetos

---
+++ pgpConsoleApp/PrimeServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first.");
+        }
+         
+    }
+}
