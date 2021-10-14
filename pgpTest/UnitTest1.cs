3b1fddfd0f5e74bcb8fffc73b3651d29fa17232c
Marcelo Salles Moniz <marcelomoniz@fiotec.fiocruz.br>
2021-10-14 08:33:20 -0300

    alteracao para testar release 111

--- pgpTest/UnitTest1.cs
+++ pgpTest/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace Prime.UnitTests.Services
 
         public PrimeService_IsPrimeShould()
         {
+            //comentario//
             _primeService = new PrimeService();
         }
 
@@ -25,6 +26,7 @@ namespace Prime.UnitTests.Services
         [DataRow(0)]
         [DataRow(1)]
         [DataRow(2)]
+        [DataRow(3)]
         //[DataRow(0)]
         //[DataRow(2)]
         public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
